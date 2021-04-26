81683ddd4351a7d234db48c0311a3b9ca9ddcb8c
Raja Aadhithan <72070645+raja-aadhithan@users.noreply.github.com>
2021-04-26 16:35:27 +0530

    top 14 final

--- Maven_verilog/Router/router_reg.v
+++ Maven_verilog/Router/router_reg.v
@@ -2,7 +2,7 @@ module router_reg(input clock, resetn, pkt_valid, fifo_full, rst_int_reg, detect
                         ld_state, laf_state, full_state, lfd_state, 
                   output reg parity_done, low_pkt_valid, err, 
                   input [7:0] data_in, output reg [7:0] dout);
-reg [7:0] a,b,c,d;
+reg [7:0] a,b,c,d,e;
 always@(posedge clock) begin
     if(!resetn) begin
         dout <= 8'd0;
@@ -10,28 +10,30 @@ always@(posedge clock) begin
         low_pkt_valid <= 1'b0;
         err <= 1'b0;
     end
-    else if(rst_int_reg) begin
-        c <= 0;
-    end
+
     else begin
     parity_done <= ((ld_state & !fifo_full & !pkt_valid) | (laf_state & low_pkt_valid & !parity_done)) & !detect_add;
     low_pkt_valid <= (ld_state & !pkt_valid);
-    if(pkt_valid) begin
-    if(detect_add) begin
-        a <= data_in;
-        c <= data_in;
+        if(pkt_valid) begin
+            if(detect_add) begin
+                dout <= data_in;
+                c <= data_in;
+            end
+            else if(ld_state||lfd_state) begin
+                if(fifo_full) b <= data_in;
+                else if(!full_state) begin
+                    c <= c^data_in;
+                    dout <= data_in;
+                end 
+            end 
+        end
+        
+        else begin
+            d <= data_in;
+            dout <= data_in;
+        end
     end
-    if(ld_state) begin
-       if(fifo_full) b <= data_in;
-       if(!full_state) c <= c^data_in;
-    end 
-    end
-    
-    else begin
-        d <= data_in;
-    end
-    end
-dout <= resetn ? (lfd_state ? a : (laf_state ? b : (ld_state ? data_in : dout))) : 8'd0;
 err <= (c==d) ? 0 : 1 ;
+//dout <= resetn ? ( lfd_state ? a : ( laf_state ? b : (ld_state ? (pkt_valid e : dout) ) ) : 8'd0;
 end
 endmodule
