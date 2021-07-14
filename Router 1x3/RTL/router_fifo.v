3447fa0c2c7961f997af0cfab497654675d463e5
Raja Aadhithan <raja.aadhithan.t@gmail.com>
2021-07-14 10:28:07 +0530

    router

--- Router 1x3/RTL/router_fifo.v
+++ Router 1x3/RTL/router_fifo.v
@@ -1,132 +1,96 @@
-module router_fifo(clk,resetn,soft_reset,write_enb,read_enb,lfd_state,datain,full,empty,dataout);
-//INPUT,OUTPUT
-input clk,resetn,soft_reset,write_enb,read_enb,lfd_state;
-input [7:0]datain;
-output reg full,empty;
-output reg [7:0]dataout;
-//internal Data types
-reg [3:0]read_ptr,write_ptr;
-reg [5:0]count;
-reg [8:0]fifo[15:0];//9 BIT DATA WIDTH 1 BIT EXTRA FOR HEADER AND 16 DEPTH SIZE
+module router_fifo #(parameter DEPTH=16, WIDTH=9, ADD_SIZE=5)
+(input clock, resetn, write_enb, read_enb, lfd_state, soft_reset, 
+ input [7:0]data_in, 
+ output reg full, empty, 
+ output reg [7:0]data_out);
+
 integer i;
-reg temp;
-reg [4:0] incrementer;
+reg [3:0]count;
+reg [8:0]mem[15:0];
+reg [ADD_SIZE-1:0]rd_add;
+reg [ADD_SIZE-1:0]wr_add;
 
-//------------------------------------------------------------------------------
-//lfd_state
-always@(posedge clk)
-	begin
-		if(!resetn)
-			temp<=1'b0;
-		else 
-			temp<=lfd_state;
-	end 
+always@(posedge clock)
+begin			
+	if(~resetn) 		//hard reset logic
+		data_out<= 0;			
+else if(soft_reset) 	// soft reset logic
+		data_out<= 8'bz;
+else if (read_enb && ~empty) 	// mem read operation
+		data_out <= mem[rd_add[3:0]][7:0];
+else if(count==0)
+		data_out<=8'bz;
+end 
 
-
-//-------------------------------------------------------------------------------------------------------------------
-//Incrementer
-
-always @(posedge clk )
+always@*		// full logic
 begin
-   if( !resetn )
-       incrementer <= 0;
-
-   else if( (!full && write_enb) && ( !empty && read_enb ) )
-          incrementer<= incrementer;
-
-   else if( !full && write_enb )
-          incrementer <=    incrementer + 1;					//inc is increased because data is written
-
-   else if( !empty && read_enb )									// inc is decrease because data is read
-          incrementer <=    incrementer - 1;
-   else
-         incrementer <=    incrementer;
+	if(~resetn )
+		full<= 1'b0;			
+else if(soft_reset)
+		full<= 1'b0;
+else if((wr_add[4]!=rd_add[4])&&(wr_add[3:0] == rd_add[3:0]))
+		full <= 1'b1;
+else
+		full <= 1'b0;				
 end
 
-//full and empty logic
-always @(incrementer)
+always@*		// empty logic
 begin
-if(incrementer==0)      //nothing in fifo
-  empty = 1 ;
-  else
-  empty = 0;
+	if(~resetn)
+		empty<= 1'b1;
+	else if(soft_reset)
+		empty<= 1'b1;
+	else if(rd_add == wr_add)
+		empty <= 1'b1;
+	else 
+		empty <= 1'b0;
+end	
 
-  if(incrementer==4'b1111)  // fifo is full
-   full = 1;
-   else
-   full = 0;
-end 
-//-----------------------------------------------------------------------------------------------------
+always@(posedge clock) 	//write address generator
+begin
+	if(~resetn)
+		wr_add<= 0;
+	else if(write_enb && ~full)
+		wr_add<=wr_add +1;
+end	
 
-//------------------------------------------------------------------------------------------------------------------------
-//Fifo write logic
-always@(posedge clk)
-	begin
-		if(!resetn || soft_reset)
-			begin
-				for(i=0;i<16;i=i+1)
-					fifo[i]<=0; 
-			end
-		
-		else if(write_enb && !full)
-				{fifo[write_ptr[3:0]][8],fifo[write_ptr[3:0]][7:0]}<={temp,datain}; //temp=1 for header data and 0 for other data
-	
-	end
+always@(posedge clock) 	//read address generator
+begin
+	if(~resetn)
+		rd_add <= 0;
+	else if(read_enb && ~empty)
+		rd_add <= rd_add +1;
+end
 
-//
-//----------------------------------------------------------------------------------------------------------------------------------------
-//FIFO READ logic
-always@(posedge clk)
-	begin
-		if(!resetn)
-			dataout<=8'd0;
+always@(posedge clock)
+begin
+	if(~resetn)
+		begin
+			 for(i=0;i<16;i=i+1)
+			 mem[i]<=0;
+		end
+	else if(soft_reset)
+		begin
+			for(i=0;i<16;i=i+1)
+			mem[i]<=0;
+		end	
+	else if(write_enb && ~full)
+		begin
+ 			if(lfd_state==1) 		// lfd_state logic
+{mem[wr_add[3:0]][8], mem[wr_add[3:0]][7:0]} <= {1'b1,data_in};
+			else 	
+{mem[wr_add[3:0]][8],mem[wr_add[3:0]][7:0]}<={1'b0,data_in};
+		end
+end		
 
-		else if(soft_reset)
-			dataout<=8'bzz;
-		
-		else
-			begin 
-				if(read_enb && !empty)
-					dataout<=fifo[read_ptr[3:0]];
-				if(count==0) // COMPLETELY READ
-					dataout<=8'bz;
-			end
-	end
-//------------------------------------------------------------------------------------------------------------------------------------
-//counter logic
-always@(posedge clk)
-	begin
-		
-		 if(read_enb && !empty)
-			begin
-				if(fifo[read_ptr[3:0]][8])                          //a header byte is read, an internal counter is loaded with the payload
-                                                               //length of the packet plus(parity byte) and starts decrementing every clock till it reached 
-					count<=fifo[read_ptr[3:0]][7:2]+1'b1;
-
-				else if(count!=6'd0)
-					count<=count-1'b1;
-				
-			end
-	
-	end
-//---------------------------------------------------------------------------------------------------------------
-//pointer logic
-always@(posedge clk)
-	begin
-		if(!resetn || soft_reset)
-			begin
-				read_ptr=5'd0;
-				write_ptr=5'd0;
-			end
-
-		else 
-			begin
-				if(write_enb && !full)
-					write_ptr=write_ptr+1'b1;
-
-				if(read_enb && !empty)
-					read_ptr=read_ptr+1'b1;
-			end
-	end
-
+always@(posedge clock) 	//loadable downcounter
+begin	
+if(read_enb && ~empty)
+begin
+		if (mem[rd_add][8]==1'b1)
+				count <= mem[rd_add[3:0]][7:2]+1'b1;
+		else if(count!=0)
+count <= count-1;
+end
+end			
 endmodule
